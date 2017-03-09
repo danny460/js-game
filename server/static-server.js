ec965b87e0fa0f732e96eb7df85c7e088e7235fd
Dannyzlh <Danielzlh62@gmail.com>
2017-03-09 21:33:29 +0800

    npm change

--- server/static-server.js
+++ server/static-server.js
@@ -1,7 +1,7 @@
 var express = require('express'),
 	app = express(),
 	port = process.env.PORT || 3000;
-app.use('/', express.static(__dirname));
+app.use('/', express.static(__dirname + "/../client"));
 app.listen(port, function(){
 	console.log('js-game listening on:', port);
 });
