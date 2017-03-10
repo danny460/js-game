d87a7cf4f65e7e9db4aff99f466a262182c6e640
zhang linghan <zhang.linghan62@gmail.com>
2017-03-10 18:28:24 +0800

    update lighting

--- client/js/demo.js
+++ client/js/demo.js
@@ -18,55 +18,68 @@
 		renderer.setClearColor(0xEEEEEE, 1.);
 		renderer.clear();
 		renderer.setSize(innerWidth, innerHeight);
+		renderer.shadowMap.enabled = true;
+		renderer.shadowMap.type = three.PCFSoftShadowMap;
 		scene = new three.Scene();
 		camera = new three.PerspectiveCamera( 75, innerWidth / innerHeight, 0.1, 1000 );		
-		camera.position.x = 0;
-		camera.position.y = 0;
-		camera.position.z = 20;
+		camera.position.set(50, 50, 50);
 		camera.lookAt(scene.position);
 
 		document.body.appendChild(renderer.domElement);
 		initMap();
-		showAxis(5000);
+		showAxis(10000);
 	}
-
-	function showAxis(length){
-		var colors = [0xff0000, 0x00ff00, 0x0000ff];
-		for(var i = 0 ; i < 3; i++){
-			var from = [0,0,0];
-			var to = [0,0,0];
-			var lineGeo = new three.Geometry();
-			var lineMat = new THREE.LineBasicMaterial({ color: colors[i] });
-			from[i] = length;
-			to[i] = -length;
-			lineGeo.vertices.push(v3.apply(null, from), v3.apply(null, to));
-			var axis = new three.Line(lineGeo, lineMat);
-			axis.type = three.Lines;
-			scene.add(axis);
-		}
-		function v3(x,y,z){
-			return new three.Vector3(x, y, z);
-		}
-	}
-
 
 	function initMap(){
-		//mapInfo
-		const light = new three.AmbientLight(0x404040, 1);
+		const DEG_2_RAD = 2 * Math.PI / 360;
+		//basis
 		const material = {
 			basicMaterial : new three.MeshBasicMaterial({ color: 0xf0f0f0 }),
 			lambertMaterial : new three.MeshLambertMaterial( { color: 0xf0f0f0 } ),
-			basePlaneMaterial : new three.MeshBasicMaterial({color: 0xffffff, side: three.DoubleSide })
+			basePlaneMaterial : new three.MeshLambertMaterial({color: 0xf0f0f0, side: three.DoubleSide })
 		}
-		var basePlaneGeometry = new three.PlaneGeometry(10, 10);
-		var basePlane = new three.Mesh(basePlaneGeometry, material.basePlaneMaterial);
-		basePlane.position.x = 0;
-		basePlane.position.y = 0;
-		basePlane.position.z = 0;
-		
-		//add to scene;
+		const light = new three.AmbientLight(0xffffff,.3);
 		scene.add(light);
-		scene.add(basePlane);
+		var pointLight = new three.PointLight(0xffffff, 1, 100, 2);
+		pointLight.position.set(0,50,0);
+		pointLight.castShadow = true;
+		pointLight.shadow.mapSize.width = 2048;  // default
+		pointLight.shadow.mapSize.height = 2048; // default
+		pointLight.shadow.camera.near = 0.5;       // default
+		pointLight.shadow.camera.far = 500      // default
+		scene.add(pointLight);
+		var basePlaneGeometry = new three.PlaneGeometry(100, 50);
+		var basePlane = new three.Mesh(basePlaneGeometry, material.basePlaneMaterial);
+		basePlane.castShadow = true;
+		basePlane.receiveShadow = true;
+		basePlane.position.set(0,0,0);
+		basePlane.rotation.set(-90 * DEG_2_RAD, 0, 0);
+		scene.add(basePlane);		
+		//adding more objects;
+		var walls = [];
+		//buffer, size, mat, pos, rot
+		addBox(walls, [10, 10, 20], material.lambertMaterial, [10,25,0], [0,0,0]);
+		addBox(walls, [10, 10, 30], material.lambertMaterial, [10,10,10], [0,90 * DEG_2_RAD, 0]);
+		// addBox(walls, [10, 10, 30], material.lambertMaterial, [10,0,10], [0,90 * DEG_2_RAD, 0]);
+		for(var i = 0; i < walls.length; i++){
+			scene.add(walls[i]);
+		}
+		var helper = new THREE.CameraHelper( pointLight.shadow.camera );
+		scene.add(helper);
+
+		function addBox(buffer, size, material, position, rotation){
+			var box = new three.Mesh(creatBoxGeo(size), material);
+			box.position.set.apply(box.position, position);
+			box.rotation.set.apply(box.rotation, rotation);
+			box.receiveShadow = true;
+			box.castShadow = true;
+			buffer.push(box);
+
+			function creatBoxGeo(args){
+				// walkaround for .apply() on constructor;
+				return new (Function.prototype.bind.apply(three.BoxGeometry, [null].concat(args)));
+			}
+		}
 	}
 
 	function animate() {
@@ -75,29 +88,39 @@
 		stats.end();
 		requestAnimationFrame( animate );
 	}
-
-	
-
 	function render(){
 		renderer.render( scene, camera );
 	}
-
+	//Camera Control and Reference
+	function showAxis(length){
+		var colors = [0xff0000, 0x00ff00, 0x0000ff];
+		for(var i = 0 ; i < 3; i++){
+			var from = [0,0,0];
+			var to = [0,0,0];
+			var lineGeo = new three.Geometry();
+			var lineMat = new THREE.LineBasicMaterial({ color: colors[i]});
+			from[i] = length;
+			to[i] = -length;
+			lineGeo.vertices.push(v3.apply(null, from), v3.apply(null, to));
+			var axis = new three.Line(lineGeo, lineMat);
+			scene.add(axis);
+		}
+		function v3(x,y,z){
+			return new three.Vector3(x, y, z);
+		}
+	}
 	function attachEventListner(){
 		window.addEventListener("resize", onWindowResize);
 		renderer.domElement.addEventListener("mousedown", onMouseDown);
 		renderer.domElement.addEventListener("mouseup", onMouseUp);
 		renderer.domElement.addEventListener("mousewheel", onScroll);
 	}
-
 	function onWindowResize() {
 		camera.aspect = innerWidth / innerHeight;
 		camera.updateProjectionMatrix();
 		renderer.setSize( innerWidth, innerHeight );
 		console.log("resized");
 	}
-
-
-
 	function onMouseDown(e){
 		e.preventDefault();
 		renderer.domElement.addEventListener("mousemove", onMouseMove);
@@ -131,7 +154,8 @@
 	}
 	function onScroll(e){
 		e.preventDefault();
-		var factor = e.wheelDelta > 0 ? 0.99 : 1.01;
+		var factor = 0.05;
+		factor = 1 + (e.wheelDelta > 0 ? -factor : factor);
 		camera.position.x *= factor;
 		camera.position.y *= factor;
 		camera.position.z *= factor;
